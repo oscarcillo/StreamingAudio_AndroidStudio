7869ab51a856b2849d2091acd6fe714370cb9313
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-02-03 17:43:16 +0100

    Initial commit

---
+++ app/src/main/java/com/otr/streamingaudio/MainActivity.java
@@ -0,0 +1,75 @@
+package com.otr.streamingaudio;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lista;
+    ArrayList<String> arrayListNombre = new ArrayList<>();
+    ArrayList<String> arrayListUrl = new ArrayList<>();
+
+    public final static String URL_CANCION = "urlcancion";
+
+    DatabaseReference db = FirebaseDatabase.getInstance().getReference("url");
+    static String URL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        //
+        lista = findViewById(R.id.listaCanciones);
+        //
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayListNombre.clear();
+                arrayListUrl.clear();
+
+                for(DataSnapshot url : dataSnapshot.getChildren()) {
+                    URL = url.getValue(String.class);
+                    arrayListUrl.add(URL);
+                    //
+                    URL = URLUtil.guessFileName(URL, null, null);
+                    URL = URL.replace(".mp3", "");
+                    arrayListNombre.add(URL);
+                }
+
+                ArrayAdapter<String> adaptador = new ArrayAdapter<>(getApplication(),android.R.layout.simple_list_item_1, arrayListNombre);
+                lista.setAdapter(adaptador);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+
+        //listener al hacer click en un item de la lista
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), SongActivity.class);
+                i.putExtra(URL_CANCION, arrayListUrl.get(position));
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/otr/streamingaudio/SongActivity.java
@@ -0,0 +1,163 @@
+package com.otr.streamingaudio;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
+
+public class SongActivity extends AppCompatActivity {
+
+    private ImageButton botonPlay;
+    private boolean play = false;
+    private TextView titulo;
+    private TextView textDuracion;
+    private TextView textCurrent;
+
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song);
+        //
+        botonPlay = findViewById(R.id.button);
+        seekBar = findViewById(R.id.seekBar);
+        textDuracion = findViewById(R.id.textDuracion);
+        titulo = findViewById(R.id.textViewTitulo);
+        textCurrent = findViewById(R.id.textViewCurrent);
+        //
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        //recuperar datos cancion
+        String url = getIntent().getStringExtra(MainActivity.URL_CANCION);
+
+        String tituloCancion = URLUtil.guessFileName(url, null, null);
+        tituloCancion = tituloCancion.replace(".mp3", "");
+        titulo.setText(tituloCancion);
+
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        seekBar.setMax(mediaPlayer.getDuration());
+
+        //
+        int tiempo = mediaPlayer.getDuration()/1000;
+        int minutos = tiempo / 60;
+        int segundos = tiempo % 60;
+
+        if(segundos<10)
+            textDuracion.setText(minutos + ":0" + segundos);
+        else if(segundos<60)
+            textDuracion.setText(minutos + ":" + segundos);
+        //
+
+        hilo h = new hilo();
+        h.start();
+
+        //listener para poder poder mover el seekbar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    mediaPlayer.seekTo(progress);
+                    //
+                    int tiempo = mediaPlayer.getCurrentPosition()/1000;
+                    int minutos = tiempo / 60;
+                    int segundos = tiempo % 60;
+
+                    ///////
+                    if(segundos<10)
+                        textCurrent.setText(minutos + ":0" + segundos);
+                    else if(segundos<60)
+                        textCurrent.setText(minutos + ":" + segundos);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void boton(View v){
+        if(!play) {
+            reproducir();
+            botonPlay.setImageResource(android.R.drawable.ic_media_pause);
+            play = true;
+        }else {
+            pausar();
+            botonPlay.setImageResource(android.R.drawable.ic_media_play);
+            play = false;
+        }
+    }
+
+    private void reproducir(){
+        mediaPlayer.start();
+    }
+
+    private void pausar(){
+        mediaPlayer.pause();
+    }
+
+    class hilo extends Thread{
+
+        @Override
+        public void run() {
+            while(true) {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                textCurrent.post(new Runnable() {
+
+                    int tiempo = mediaPlayer.getCurrentPosition()/1000;
+                    int segundos = 0;
+                    int minutos = 0;
+
+                    @Override
+                    public void run() {
+
+                        minutos = tiempo / 60;
+                        segundos = tiempo % 60;
+
+                        ///////
+                        if(segundos<10)
+                            textCurrent.setText(minutos + ":0" + segundos);
+                        else if(segundos<60)
+                            textCurrent.setText(minutos + ":" + segundos);
+                    }
+                });
+                try{
+                    sleep(1000);
+                }catch (Exception e){
+
+                }
+
+            }
+        }
+    }
+
+}
